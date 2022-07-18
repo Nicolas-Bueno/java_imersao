4eecae84a7b994ff1937e82c8f875c0d6b0a0fb7
Nicolas Bueno <nicolasbrian202@gmail.com>
2022-07-18 12:42:38 -0300

    Fazendo conexão HTTP e buscando os filmes

---
+++ src/App.java
@@ -0,0 +1,22 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class App {
+    public static void main(String[] args) throws Exception{
+        // Fazer conexão HTTP e buscar os filmes
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_s0xlqz72";
+        URI address = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(address).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+    
+    
+    
+    }
+}
+
