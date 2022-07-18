37a932fe403236412767f1ad8a6db0b1981569c3
Nicolas Bueno <nicolasbrian202@gmail.com>
2022-07-18 15:52:40 -0300

    extraindo dados

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception{
@@ -14,8 +16,11 @@ public class App {
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
         System.out.println(body);
-    
-    
+        
+        //extrair só os dados
+        JsonParse parser = new JsonParse();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        
     
     }
 }
